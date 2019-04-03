6a579ef9fa1cc0278e1e77a508c2b900cc22da1a
Test <thompson.doan@gmail.com>
2019-04-03 18:07:27 +1100

    Add Route and mobX appState to App.tsx
    - 3 routes added to App.tsx. Picture of the day (fetch potd from NASA API), Gallery (fetch potd with date controller to view potd from another date), Random (reveal basic form created earlier).
    - init mobX store and pass appState to POTD component routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, ReactNode } from "react";
+import { Route, Link } from "react-router-dom";
+import Store from "./store/Store";
+import "./App.css";
+import Apod from "./pages/apod/apod";
+import ImageTileTitle from "./components/imageTileTitle/imageTileTitle";
+import Form from "./components/Form/Form";
 
-class App extends Component {
-  render() {
+interface Props {
+  appState?: Store;
+}
+
+class App extends Component<Props> {
+  public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Link to="/">Home</Link>
+        <Route
+          path="/"
+          exact
+          render={props => (
+            <React.Fragment>
+              <Link to="/apod">
+                <ImageTileTitle
+                  classNames="imageTileTitle--offwhite"
+                  imgUrl="https://apod.nasa.gov/apod/image/1903/NGC3324_SHO_1024.jpg"
+                  title="PICTURE OF THE DAY"
+                  centerText
+                  backdrop
+                  bottomAlign
+                />
+              </Link>
+              <Link to="/gallery">
+                <ImageTileTitle
+                  classNames="imageTileTitle--offwhite"
+                  imgUrl="https://apod.nasa.gov/apod/image/1901/vltlaser_beletsky_960.jpg"
+                  title="GALLERY"
+                  centerText
+                  backdrop
+                  bottomAlign
+                />
+              </Link>
+              <Link to="/random">
+                <ImageTileTitle
+                  classNames="imageTileTitle--offwhite"
+                  imgUrl="https://apod.nasa.gov/apod/image/1701/SpaceXLaunch_Shortt_960.jpg"
+                  title="RANDOM"
+                  centerText
+                  backdrop
+                  bottomAlign
+                />
+              </Link>
+            </React.Fragment>
+          )}
+        />
+        {/* <Route path="/apod" component={Apod} /> */}
+        <Route
+          path="/apod"
+          render={(props): any => (
+            <Apod {...props} appState={this.props.appState} />
+          )}
+        />
+        <Route
+          path="/gallery"
+          render={(props): any => (
+            <Apod {...props} isDateController appState={this.props.appState} />
+          )}
+        />
+        <Route path="/random" component={Form}/>
+      </>
     );
   }
 }
